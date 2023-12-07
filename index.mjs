31821187f3079cc8d747ad8fd870416d36175f09
John <john@iamjohnhenry.com>
2023-12-07 13:40:12 -0800

    put secret in correct place

--- index.mjs
+++ index.mjs
@@ -1,11 +1,9 @@
 import { Server } from "npm:proxy-socks@0.0.3";
 const secret = Deno.env.get("SECRET");
-const server = new Server(
-  () => new Response("no responder", { status: 500, secret }),
-  {
-    strategy: "random",
-  }
-);
+const server = new Server(() => new Response("no responder", { status: 500 }), {
+  strategy: "random",
+  secret,
+});
 console.log({ secret });
 Deno.serve((req) => {
   if (req.headers.get("upgrade") !== "websocket") {
