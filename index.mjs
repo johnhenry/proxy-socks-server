d6ad3f40d1bcc979b9470d5818e017178bfbbfb7
John <john@iamjohnhenry.com>
2023-12-07 13:04:02 -0800

    Update to proxy-socks 0.0.3 and add secret

--- index.mjs
+++ index.mjs
@@ -1,7 +1,11 @@
-import { Server } from "npm:proxy-socks@0.0.2";
-const server = new Server(() => new Response("no responder", { status: 500 }), {
-  strategy: "random",
-});
+import { Server } from "npm:proxy-socks@0.0.3";
+const secret = Deno.env.get("SECRET");
+const server = new Server(
+  () => new Response("no responder", { status: 500, secret }),
+  {
+    strategy: "random",
+  }
+);
 Deno.serve((req) => {
   if (req.headers.get("upgrade") !== "websocket") {
     return server.fetch(req);
